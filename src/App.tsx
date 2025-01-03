075c110a37e8d94af3beb22eafac04a9053cc085
Massa Timothée <timothee.massa.perso@gmail.com>
2025-01-03 18:01:15 +0100

    feat(ui): create question card component and add it to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import questionData from './data/questions.json'
+import { Question } from './types/Question'
+import QuestionCard from './components/QuestionCard.tsx'
 
 export default function App() {
+    const question: Question = questionData
+
     return (
         <React.StrictMode>
-            <main>
-                
+            <main className="main--container">
+                <QuestionCard data={question} />
             </main>
         </React.StrictMode>
     )
