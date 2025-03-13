983ae5727ac69581c659f3f1b9f533291beda4cf
Massa Timothée <timothee.massa.perso@gmail.com>
2025-03-13 21:10:51 +0100

    refactor: use repo for cyberun

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import questionData from './data/questions.json'
-import { Question } from './types/Question'
-import QuestionCard from './components/QuestionCard.tsx'
 
 export default function App() {
-    const question: Question = questionData
-
     return (
         <React.StrictMode>
-            <main className="main--container">
-                <QuestionCard data={question} />
-            </main>
         </React.StrictMode>
     )
 }
--- src/types/Question.ts
+++
@@ -1,7 +0,0 @@
-export interface Question {
-    question: string;
-    answers: {
-        yes: string;
-        no: string;
-    };
-}
