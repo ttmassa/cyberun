200ac75ad3acb082130d4630d7cd394555d04088
Massa Timothée <timothee.massa.perso@gmail.com>
2025-03-14 17:42:11 +0100

    feat: create another set of schema/uishcema/data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { JsonForms } from '@jsonforms/react'
+import { schema1, uischema1, data1 } from './data/index.ts'
 
 export default function App() {
     return (
         <React.StrictMode>
+            <div>
+                <JsonForms 
+                    schema={schema1}
+                    uischema={uischema1}
+                    data={data1}
+                    renderers={[
+                        
+                    ]}
+                />
+            </div>
         </React.StrictMode>
     )
 }
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,9 @@
-export { default as schema } from './shema.json';
-export { default as uischema } from './uishema.json';
-export { default as data } from './data.json';
+// Set1
+export { default as schema1 } from './set1/schema.json';
+export { default as uischema1 } from './set1/uischema.json';
+export { default as data1 } from './set1/data.json';
+
+// Set2
+export { default as schema2 } from './set2/schema.json';
+export { default as uischema2 } from './set2/uischema.json';
+export { default as data2 } from './set2/data.json';
